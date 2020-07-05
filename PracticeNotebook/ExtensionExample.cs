fbf5e172cf014f87fef6ea4f38a034d0f99d1755
SiqiWang666 <siqi_astrid@gwu.edu>
2020-07-05 19:08:53 -0400

    Extension methods (#4).

--- PracticeNotebook/ExtensionExample.cs
+++ PracticeNotebook/ExtensionExample.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace PracticeNotebook
 {
     static class ExtensionExample
@@ -5,12 +7,12 @@ namespace PracticeNotebook
         /*
          * Extension method is way to add a new functionality into an existing type.
          * requirements:
-         *  1. class must be static
+         *  1. class must be static, non-nested, non-generic
          *  2. extension method itself must be static
          *  3. first param to the extension method must be of the type which will be extended and with leading `this` keyword.
          */
 
-        public static bool IsPrime(this int x, string a)
+        public static bool IsPrime(this int x)
         {
             if (x < 4 && x > 0) return true;
             for (int i = 2; i < x / 2; i++)
@@ -20,5 +22,12 @@ namespace PracticeNotebook
             
             return true;
         }
+
+        // Extension methods can be added to any types
+        public static List<T> CombineLists<T>(List<T> a, List<T> b)
+        {
+            a.AddRange(b);
+            return a;
+        } 
     }
 }
