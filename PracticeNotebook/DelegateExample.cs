8329d81c499d16b5c358a7a4426d2edc176b0fd1
SiqiWang666 <siqi_astrid@gwu.edu>
2020-07-06 01:02:09 -0400

    Delegate with lambda expression (#4).

--- PracticeNotebook/DelegateExample.cs
+++ PracticeNotebook/DelegateExample.cs
@@ -4,15 +4,33 @@ namespace PracticeNotebook
 {
     public class DelegateExample
     {
+        /*
+         * Delegate is a pointer to a method which can be passed as a parameter.
+         */
         public void ActionDelegateExample()
         {
-            // Action<int> factorial = new Action<int>(CalculateFactorial);
-            Action<int> factorial = delegate(int a)
+            // define a delegate function
+            Action<int> factorialImplement = new Action<int>(CalculateFactorial);
+            
+            // Using anonymous methods
+            Action<int> factorialAnonymous = delegate(int a)
             {
                 int fact = 1;
                 for (int i = a; i > 0; i--)
                 {
                     fact *= fact;
+                }
+            
+                Console.WriteLine(fact);
+            };
+            
+            // Using lambda expression
+            Action<int> factorialLambda = (a) =>
+            {
+                var fact = 1;
+                for (int i = a; i > 0; i--)
+                {
+                    fact *= i;
                 }
 
                 Console.WriteLine(fact);
@@ -49,6 +67,21 @@ namespace PracticeNotebook
             if (n == 1) return 1;
             return GetFibonacci(n - 1) + GetFibonacci(n - 2);
         }
+
+        public void IncrementHelper(int[] arr, MathDelegate worker)
+        {
+            int increment = 99;
+            foreach (var element in arr)
+            {
+                worker(element, increment);
+            }
+        }
+
+        // Invoke the customized delegate
+        public void Increment(int[] arr)
+        {
+            IncrementHelper(arr, (a, b) => Console.WriteLine(a + b));
+        }
     }
     
     /*
@@ -56,7 +89,10 @@ namespace PracticeNotebook
      * Action: takes any type of input parameter but does not return any value
      * Predicate: takes any type of input but returns bool value
      * Func: takes any type of input params and can return any type of value
+     *
+     * FYI: It is less common to customize a delegate.
      */
-    // It is less common to customize a delegate.
-    //public delegate void MathDelegate(int a, int b);
+    
+    // Customize a new delegate type
+    public delegate void MathDelegate(int a, int b);
 }
