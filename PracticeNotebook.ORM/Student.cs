4b9c111ae7d3bf8f0443de2dd0229de3ef95dd85
SiqiWang666 <siqi_astrid@gwu.edu>
2020-07-07 14:24:20 -0400

    Dapper samples (#5).

---
+++ PracticeNotebook.ORM/Student.cs
@@ -0,0 +1,9 @@
+namespace PracticeNotebook.ORM
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string SName { get; set; }
+        public string Mobile { get; set; }
+    }
+}
